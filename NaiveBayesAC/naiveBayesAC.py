bff4482c9a951bb094ae5994639b43a2e7882a3d
AbinashChetia <abinashdu2000@gmail.com>
2023-10-12 16:59:40 +0530

    Updated

--- NaiveBayesAC/naiveBayesAC.py
+++ NaiveBayesAC/naiveBayesAC.py
@@ -4,10 +4,32 @@ import pandas as pd
 class NaiveBayesAC:
     def __init__(self):
         self.classes = None
-        self.prob = None
-        self.llhd = None
+        self.cls_prob = None
+        self.likelihood = None
 
     def fit(self, X, y):
-        pass
+        self.classes = np.unique(y)
+        self.cls_prob = {}
+        self.likelihood = {}
 
+        for cls in self.classes:
+            self.cls_prob[cls] = X[y == cls].shape[0] / X.shape[0]
+            self.likelihood[cls] = {}
+            
+            for col in X.columns:
+                self.likelihood[cls][col] = {}
+                for val in np.unique(X[col]):
+                    self.likelihood[cls][col][val] = X[(X[col] == val) & (y == cls)].shape[0] / X[y == cls].shape[0]
+
+    def predict(self, X):
+        return [self._pred_row(X.iloc[i, :]) for i in range(X.shape[0])]
+    
+    def _pred_row(self, x):
+        posteriors = []
+        for cls in self.classes:
+            likelihood = 1
+            for col in x.index:
+                likelihood *= self.likelihood[cls][col][x[col]]
+            posteriors.append(self.cls_prob[cls] * likelihood)
+        return self.classes[np.argmax(posteriors)]
 
---
+++ NaiveBayesAC/utilsAC.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+
+def splitTrainTest(x, y, train_ratio=0.8):
+    '''
+    Split data into training and testing sets.
+    '''
+    df_x = x.copy()
+    df_y = y.copy()
+    df_y = df_y.rename('y')
+    df = pd.concat([df_x, df_y], axis=1)
+    df = df.sample(frac=1).reset_index(drop=True)
+    train_size = int(len(df) * train_ratio)
+    train_x = df.iloc[:train_size, :-1].reset_index(drop=True)
+    train_y = df.iloc[:train_size, -1].reset_index(drop=True)
+    test_x = df.iloc[train_size:, :-1].reset_index(drop=True)
+    test_y = df.iloc[train_size:, -1].reset_index(drop=True)
+    return train_x, train_y, test_x, test_y
+
+def split_kfold(x, y, k=5):
+    '''
+    Split data into training and testing sets for k-fold cross validation.
+    '''
+    df_x = x.copy()
+    df_y = y.copy()
+    df_y = df_y.rename('y')
+    df = pd.concat([df_x, df_y], axis=1)
+    df = df.sample(frac=1).reset_index(drop=True)
+    fold_size = int(len(df) / k)
+    data_folds = []
+    for i in range(k):
+        if i != k - 1:
+            data_folds.append(df.iloc[i * fold_size: (i + 1) * fold_size, :].reset_index(drop=True))
+        else:
+            data_folds.append(df.iloc[i * fold_size:, :].reset_index(drop=True))
+    return data_folds
+    
+
+def normMinMax(df, mode='train', train_min=None, train_max=None):
+    '''
+    Perform min-max normalization on data.
+    '''
+    data = df.copy()
+    if mode == 'train':
+        train_max = {}
+        train_min = {}
+        for col in data.columns:
+            train_max[col] = data[col].max()
+            train_min[col] = data[col].min()
+            data[col] = (data[col] - train_min[col]) / (train_max[col] - train_min[col])
+        return data, train_min, train_max
+    
+    elif mode == 'test':
+        if train_min is None or train_max is None:
+            raise Exception('Pass train_min and/or train_max.')
+        for col in data.columns:
+            data[col] = (data[col] - train_min[col]) / (train_max[col] - train_min[col])
+        return data

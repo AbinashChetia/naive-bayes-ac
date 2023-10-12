dab8ea401aaabcebde9cd9cf019a27528c06c7b5
AbinashChetia <abinashdu2000@gmail.com>
2023-10-12 16:01:40 +0530

    Updated

---
+++ NaiveBayesAC/naiveBayesAC.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+class NaiveBayesAC:
+    def __init__(self):
+        self.classes = None
+        self.prob = None
+        self.llhd = None
+
+    def fit(self, X, y):
+        pass
+
+
